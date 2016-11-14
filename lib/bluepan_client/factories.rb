d7fea639d22b43aa53de6f38606c9f3805c55b79
Ramon Tayag <ramon.tayag@gmail.com>
2016-11-14 13:49:21 +0800

    Add BluepanClient::Strategy to factories

--- lib/bluepan_client/factories.rb
+++ lib/bluepan_client/factories.rb
@@ -13,4 +13,14 @@ FactoryGirl.define do
     cityname_cn "Chinese Name"
   end
 
+  factory :bluepan_client_strategy, class: "BluepanClient::Strategy" do
+    sequence(:strategyseq) { |n| n }
+    currency { %w(KRW CNY).sample }
+    name "Local Name"
+    name_en "English Name"
+    payoutmethod "2"
+    strategy { %w(bank alipay wechatpay payment).sample }
+    maximum 200_000
+  end
+
 end
