7de3020ccdc40c9d00ca164eb92af477205febca
Ramon Tayag <ramon.tayag@gmail.com>
2017-06-20 17:20:18 +0200

    Complete spec to test that BTCCNY exists

--- spec/acceptance/get_exchange_rates_spec.rb
+++ spec/acceptance/get_exchange_rates_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe "#get_exchange_rates", vcr: {record: :once} do
 
   it "returns the list of exchange_rates" do
     client = BluepanClient.new(CONFIG)
-    client.get_exchange_rates
+    response = client.get_exchange_rates
+
+    btccny = response.exchange_rates.find {|e| e.currency_pair == "BTCCNY"}
+    expect(btccny.rate > 0).to be true
   end
 
 end
